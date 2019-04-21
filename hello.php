8dd0a5931961e5dac15a71224293a7089f821b21
Bas de Zeeuw <bas.de.zeeuw@sentia.com>
2019-04-21 19:19:40 +0200

    adjust tst

--- hello.php
+++ hello.php
@@ -5,8 +5,18 @@
  <body>
  <p>Hello World</p>
      <?php
-    $homepage = file_get_contents('https://kibana.k8s.vestas-poc.demo.sentia.net/api/status');
-    echo $homepage;
+/* gets the data from a URL */
+    function get_data($url) {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+    $returned_content = get_data('https://kibana.k8s.vestas-poc.demo.sentia.net/api/status');
     ?>
  </body>
 </html>
